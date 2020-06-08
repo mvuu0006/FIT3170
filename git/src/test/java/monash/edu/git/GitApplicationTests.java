c655ab0213eff2329d7f7055ac66bf2eb9989ac0
Keshav Seshadri <kses0002@student.monash.edu>
2020-06-08 18:09:17 +1000

    Set up commit

---
+++ git/src/test/java/monash/edu/git/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package monash.edu.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
