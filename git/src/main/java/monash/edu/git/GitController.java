20d019fa808e92bafbf450795cb2f97b94ae3e49
Keshav Seshadri <kses0002@student.monash.edu>
2020-06-09 02:41:37 +1000

    Calculating contribution percent of each team member

--- git/src/main/java/monash/edu/git/GitController.java
+++ git/src/main/java/monash/edu/git/GitController.java
@@ -1,6 +1,8 @@
 package monash.edu.git;
 
+import netscape.javascript.JSObject;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,9 @@ public class GitController {
 
     GitService gitService;
 
+    // Method responsible for getting all the contributors and their contribution percent
     @RequestMapping("/contributors")
-    public List<String> getContributions(String apiUrl) throws IOException, JSONException {
+    public JSONObject getContributions(String apiUrl) throws IOException, JSONException {
         return gitService.getContributions();
     }
 }
--- git/src/main/java/monash/edu/git/GitService.java
+++ git/src/main/java/monash/edu/git/GitService.java
@@ -1,32 +1,47 @@
 package monash.edu.git;
 
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class GitService {
     String gitUrl="https://api.github.com/projects/1002604";
 
-
-    public List<String> getContributions() throws IOException, JSONException {
-
-        List<String> commitList = new ArrayList<String>();
+    public JSONObject getContributions() throws IOException, JSONException {
+        //Creating a JSONObject that stores all the contributions
+        JSONObject contribution = new JSONObject();
 
         // Original URL : https://github.com/tensorflow/tensorflow.git
-        String reposUrl="https://api.github.com/repos/tensorflow/tensorflow/contributors";
+        String reposUrl="https://api.github.com/repos/tensorflow/tensorflow/contributors?per_page=500";
 
-        // Class that reads from a URL
+        // Class that reads from a URL and returns info in JSON format
         GetJSONReader jsonReader= new GetJSONReader();
         JSONObject json = jsonReader.readJsonFromUrl(reposUrl);
 
-        return commitList;
+        // Extracting the array from the JSON Object
+        JSONArray jsonArray = json.getJSONArray("entry");
+
+        // Calculating the total cotributions
+        int totalContributions=0;
+        for(int i=0;i<jsonArray.length();i++)
+        {
+            totalContributions=totalContributions+jsonArray.getJSONObject(i).getInt("contributions");
+        }
+
+        // Calculating the contribution percentage of each team member in the repo
+        for(int i=0;i<jsonArray.length();i++)
+        {
+            int individualContribution=jsonArray.getJSONObject(i).getInt("contributions");
+            double contributionPercent=((double)individualContribution/totalContributions)*100;
+            contributionPercent=Math.round(contributionPercent*100.0)/100.0;
+            String name=jsonArray.getJSONObject(i).getString("login");
+
+            contribution.put(name,contributionPercent);
+        }
+
+        return contribution;
     }
 }
