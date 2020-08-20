34624a083451a706724f6c4a467dbb7a0ae0e573
Keshav Seshadri <kses0002@student.monash.edu>
2020-08-21 01:06:13 +1000

    Number of open issues from each user extracted from GitLab API

--- git/src/main/java/monash/edu/git/GitLabRepository.java
+++ git/src/main/java/monash/edu/git/GitLabRepository.java
@@ -9,12 +9,45 @@ import java.io.IOException;
 public class GitLabRepository {
     private String id;
     private JSONObject commits;
+    private JSONObject issues;
 
     public GitLabRepository(String id) throws IOException, JSONException {
         this.id=id;
 
         String repoUrl="https://gitlab.com/api/v4/projects/"+id;
         constructRepoCommits(repoUrl);
+        constructRepoIssues(repoUrl);
+
+    }
+
+    public void constructRepoIssues(String reposUrl) throws IOException, JSONException {
+        String issuesUrl=reposUrl+"/issues";
+        issues=new JSONObject();
+
+        GetJSONReader jsonReader= new GetJSONReader();
+        JSONObject json = jsonReader.readJsonFromUrl(issuesUrl);
+
+        JSONArray jsonArray = json.getJSONArray("entry");
+
+        for (int i=0; i<jsonArray.length();i++)
+        {
+            String issue_type=jsonArray.getJSONObject(i).getString("state");
+
+            if(issue_type.equals("opened"))
+            {
+                String author = jsonArray.getJSONObject(i).getJSONObject("author").getString("name");
+                if (issues.has(author))
+                {
+                    int issue_count=issues.getInt(author) + 1;
+                    issues.put(author,issue_count);
+                }
+                else
+                {
+                    issues.put(author,1);
+                }
+            }
+        }
+        int a=0;
 
     }
 
