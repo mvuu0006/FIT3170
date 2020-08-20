89e77012926e84024c325256cb0b9300592f4d5d
Keshav Seshadri <kses0002@student.monash.edu>
2020-08-20 23:03:53 +1000

    Commit count and commit author extracted from GitLab API

--- git/src/main/java/monash/edu/git/GitApplication.java
+++ git/src/main/java/monash/edu/git/GitApplication.java
@@ -1,13 +1,17 @@
 package monash.edu.git;
 
+import org.json.JSONException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class GitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitApplication.class, args);
+	public static void main(String[] args) throws IOException, JSONException {
+		GitLabRepository gitLabRepository = new GitLabRepository("3398900");
+		//SpringApplication.run(GitApplication.class, args);
 	}
 
 }
--- git/src/main/java/monash/edu/git/GitLabRepository.java
+++ git/src/main/java/monash/edu/git/GitLabRepository.java
@@ -1,21 +1,46 @@
 package monash.edu.git;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class GitLabRepository {
     private String id;
+    private JSONObject commits;
 
-    public GitLabRepository(String id)
-    {
+    public GitLabRepository(String id) throws IOException, JSONException {
         this.id=id;
 
         String repoUrl="https://gitlab.com/api/v4/projects/"+id;
         constructRepoCommits(repoUrl);
 
-
     }
 
-    public void constructRepoCommits(String repoUrl)
-    {
+    public void constructRepoCommits(String repoUrl) throws IOException, JSONException {
+        String commitsUrl=repoUrl+"/repository/commits";
+        commits = new JSONObject();
 
+        GetJSONReader jsonReader= new GetJSONReader();
+        JSONObject json = jsonReader.readJsonFromUrl(commitsUrl);
+
+        JSONArray jsonArray = json.getJSONArray("entry");
+
+        for (int i=0; i<jsonArray.length();i++)
+        {
+            String name=jsonArray.getJSONObject(i).getString("author_name");
+
+            if (commits.has(name))
+            {
+                int commits_count=commits.getInt(name) + 1;
+                commits.put(name,commits_count);
+            }
+            else
+            {
+                commits.put(name,1);
+            }
+        }
     }
 
 }
