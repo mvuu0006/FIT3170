b2ee0cf146a65b6075b7af466eab6645e9f691f1
Hayden <hbaker@outlook.com.au>
2020-10-04 17:58:29 +1100

    began creating new git controller

--- git/src/main/java/monash/edu/git/GitControllerNew.java
+++ git/src/main/java/monash/edu/git/GitControllerNew.java
@@ -1,5 +1,49 @@
 package monash.edu.git;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/git")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003"}, maxAge = 0)
 public class GitControllerNew {
-    
+    /*
+     PUT: New project into database
+     */
+    @PutMapping(path = "/project")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    @ResponseBody
+    public void putProject(@RequestParam("project-id") String id,
+        @RequestParam("email") String email) throws ForbiddenException, JSONException, ClassNotFoundException {
+        // Check that query params arent empty
+        if( !id.equals("") | !email.equals("") ) {
+            throw new ForbiddenException();
+        }
+        // Add to database
+        String addScript = "INSERT INTO gitdb.Project(projectName, email) VALUES ("+name+"\',\'"+email+"\')"; // TODO: Fix this mapping to take project name from request body
+        int rowsChanged = dbHandler.executeUpdate(addScript);
+        // Throw error if row wasn't added
+        if (rowsChanged == 0) {
+            throw new ForbiddenException();
+        }
+    }
+
 }
